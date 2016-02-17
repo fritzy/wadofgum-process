7712dce6af12a51981ab66ca09a06706e9660fc4
Nathan Fritz <fritzy@netflint.net>
2016-02-17 12:39:13 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const lodash = require('lodash');
+const isPromise = require('is-promise');
+
+function Processors(baseClass) {
+
+  class Model extends baseClass {
+
+    processCollection (objs, tags) {
+
+      let pms = [];
+      for (const obj of objs) {
+        pms.push(this.process(objs, tags));
+      }
+      return Promise.all(pms);
+    }
+
+    process (obj, tags) {
+      tags = Array.isArray(tags) ? tags : [tags];
+
+      const processors = this.constructor.meta.get('processors');
+
+      const promises = [];
+      const promiseMap = [];
+      const out = loaddash.cloneDeep(obj);
+
+      for (let tag of tags) {
+        promises[tag] = {}
+        for (let field of Object.keys(processors[tag] || {})) {
+          let pm = processors[tag][field](obj[field], obj);
+          if (!isPromise(pm)) {
+            pm = Promise.resolve(pm);
+          }
+          promises.push(pm);
+          promiseMap.push(field);
+        }
+      }
+      return Promise.all(promises)
+      .then((values) => {
+
+        for (let value of values) {
+          const field = promiseMap.shift();
+          out[field] = value;
+        }
+        return Promise.resolve(this);
+      });
+    }
+
+  }
+
+  Model.capabilities.add('processors');
+  return Model;
+}
+
